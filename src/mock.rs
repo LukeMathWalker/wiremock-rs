a054d721b556422df9e3007d8a84011fb924f155
Luca Palmieri <lpalmieri@truelayer.com>
2020-05-11 15:04:33 +0100

    Refactor again the Times structure. Add a failing test.

--- src/mock.rs
+++ src/mock.rs
@@ -2,7 +2,7 @@ use crate::response_template::ResponseTemplate;
 use crate::{MockServer, Request};
 use http_types::Response;
 use std::fmt::{Debug, Formatter};
-use std::ops::{Range, RangeBounds, RangeFrom, RangeInclusive, RangeTo, RangeToInclusive};
+use std::ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive};
 
 /// Anything that implements `Match` can be used to constrain when a [`Mock`] is activated.
 ///
@@ -218,6 +218,7 @@ pub struct Mock {
     // If `Some(max_n_matches)`, when `max_n_matches` matching incoming requests have been processed,
     // `self.matches` should start returning `false`, regardless of the incoming request.
     pub(crate) max_n_matches: Option<u64>,
+    pub(crate) expectation: Times,
 }
 
 /// A fluent builder to construct a [`Mock`] instance given matchers and a [`ResponseTemplate`].
@@ -241,9 +242,9 @@ impl Mock {
         }
     }
 
-    /// Specify the maximum number of times you would like this `Mock` to respond to
+    /// Specify an upper limit to the number of times you would like this `Mock` to respond to
     /// incoming requests that satisfy the conditions imposed by your [`matchers`].
-    ///
+    /// 
     /// ### Example:
     ///
     /// ```rust
@@ -291,6 +292,67 @@ impl Mock {
     pub fn up_to_n_times(mut self, n: u64) -> Mock {
         assert!(n > 0, "n must be strictly greater than 0!");
         self.max_n_matches = Some(n);
+        self
+    }
+
+    /// Set an expectation on the number of times this `Mock` should match in the current
+    /// test case.
+    /// Expectations are verified when the [`MockServer`] is shutting down: if the expectation
+    /// is not satisfied, the [`MockServer`] will panic.
+    ///
+    /// By default, no expectation is set for `Mock`s.
+    ///
+    /// ### When is this useful?
+    ///
+    /// `expect` can turn out handy when you'd like to verify that a certain side-effect has
+    /// (or has not!) taken place.
+    ///
+    /// For example:
+    /// - check that a 3rd party notification API (e.g. email service) is called when an event
+    ///   in your application is supposed to trigger a notification;
+    /// - check that a 3rd party API is NOT called when the response of a call is expected
+    ///   to be retrieved from a cache (`.expect(0)`).
+    ///
+    /// This technique is also called [spying](https://martinfowler.com/bliki/TestDouble.html).
+    ///
+    /// ### Example:
+    ///
+    /// ```rust
+    /// use wiremock::{MockServer, Mock, ResponseTemplate};
+    /// use wiremock::matchers::method;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     // Arrange
+    ///     let mock_server = MockServer::start().await;
+    ///
+    ///     Mock::given(method("GET"))
+    ///         .respond_with(ResponseTemplate::new(200))
+    ///         .up_to_n_times(2)
+    ///         // We expect the mock to be called at least once.
+    ///         // If that does not happen, the `MockServer` will panic on shutdown,
+    ///         // causing the whole test to fail.
+    ///         .expect(1..)
+    ///         .mount(&mock_server)
+    ///         .await;
+    ///     
+    ///     // Act
+    ///     let status = surf::get(&mock_server.uri())
+    ///         .await
+    ///         .unwrap()
+    ///         .status();
+    ///     assert_eq!(status.as_u16(), 200);
+    ///
+    ///     // Assert
+    ///     // We made at least one matching request, the expectation is satisfied.
+    ///     // The `MockServer` will shutdown peacefully, without panicking.
+    /// }
+    /// ```
+    ///
+    /// [`matchers`]: matchers/index.html
+    pub fn expect<T: Into<Times>>(mut self, r: T) -> Mock {
+        let range = r.into();
+        self.expectation = range;
         self
     }
 
@@ -344,65 +406,89 @@ impl MockBuilder {
             matchers: self.matchers,
             response: template,
             max_n_matches: None,
+            expectation: Times(TimesEnum::Unbounded(RangeFull)),
         }
     }
 }
 
-/// Specify how many times a [`Mock`] should match.
+/// Specify how many times we expect a [`Mock`] to match.
 /// It is used to set expectations on the usage of a [`Mock`] in a test case.
 ///
 /// You can either specify an exact value, e.g.
 /// ```rust
 /// use wiremock::Times;
 ///
-/// let times: Times<_> = 10.into();
+/// let times: Times = 10.into();
 /// ```
 /// or a range
 /// ```rust
 /// use wiremock::Times;
 ///
 /// // Between 10 and 15 (not included) times
-/// let times: Times<_> = (10..15).into();
+/// let times: Times = (10..15).into();
 /// // Between 10 and 15 (included) times
-/// let times: Times<_> = (10..=15).into();
+/// let times: Times = (10..=15).into();
 /// // At least 10 times
-/// let times: Times<_> = (10..).into();
+/// let times: Times = (10..).into();
 /// // Strictly less than 15 times
-/// let times: Times<_> = (..15).into();
+/// let times: Times = (..15).into();
 /// // Strictly less than 16 times
-/// let times: Times<_> = (..=15).into();
+/// let times: Times = (..=15).into();
 /// ```
 ///
 /// [`Mock`]: struct.Mock.html
-pub struct Times<R: RangeBounds<u64>>(R);
+#[derive(Debug)]
+pub struct Times(TimesEnum);
 
-// Implementation notes: the original draft used an enum with two variants (Exact and Range)
-// instead of a struct wrapped around a generic range.
-// We switched to a range wrapper when we realised that you would have had to specify a range
-// type when creating the exact variant (e.g. as you do for `Option` when creating a `None` variant).
+// Implementation notes: this has gone through a couple of iterations before landing to
+// what you see now.
 //
-// We achieve the same functionality with a wrapper, but exact values have to converted to ranges
-// with a single element (e.g. 15 -> 15..16).
-// Not the most expressive representation, but we will have to live with it.
-impl From<u64> for Times<Range<u64>> {
+// The original draft had Times itself as an enum with two variants (Exact and Range), with
+// the Range variant generic over `R: RangeBounds<u64>`.
+//
+// We switched to a generic struct wrapper around a private `R: RangeBounds<u64>` when we realised
+// that you would have had to specify a range type when creating the Exact variant
+// (e.g. as you do for `Option` when creating a `None` variant).
+//
+// We achieved the same functionality with a struct wrapper, but exact values had to converted
+// to ranges with a single element (e.g. 15 -> 15..16).
+// Not the most expressive representation, but we would have lived with it.
+//
+// We changed once again when we started to update our `MockActor`: we are storing all `Mock`s
+// in a vector. Being generic over `R`, the range type leaked into the overall `Mock` (and `ActiveMock`)
+// type, thus making those generic as well over `R`.
+// To store them in a vector all mocks would have had to use the same range internally, which is
+// obviously an unreasonable restrictions.
+// At the same time, we can't have a Box<dyn RangeBounds<u64>> because `contains` is a generic
+// method hence the requirements for object safety are not satisfied.
+//
+// Thus we ended up creating this master enum that wraps all range variants with the addition
+// of the Exact variant.
+// If you can do better, please submit a PR.
+// We keep them enum private to the crate to allow for future refactoring.
+#[derive(Debug)]
+pub(crate) enum TimesEnum {
+    Exact(u64),
+    Unbounded(RangeFull),
+    Range(Range<u64>),
+    RangeFrom(RangeFrom<u64>),
+    RangeTo(RangeTo<u64>),
+    RangeToInclusive(RangeToInclusive<u64>),
+    RangeInclusive(RangeInclusive<u64>),
+}
+
+impl From<u64> for Times {
     fn from(x: u64) -> Self {
-        Times(x..x + 1)
+        Times(TimesEnum::Exact(x))
     }
 }
 
-// We can't use a single `impl<R: RangeBounds<u64>> From<R> for Times<R>` because of the orphan
-// rule - we end up with a conflicting implementation to the conversion we implemented above
-// for u64.
-// A quick macro to help easing the pain.
-//
-// Important: we do not provide a conversion for RangeFull given that we never expect the user
-// to pass it in considering it is the default behaviour of a mock when no times quantifier
-// is specific.
+// A quick macro to help easing the implementation pain.
 macro_rules! impl_from_for_range {
     ($type_name:ident) => {
-        impl From<$type_name<u64>> for Times<$type_name<u64>> {
+        impl From<$type_name<u64>> for Times {
             fn from(r: $type_name<u64>) -> Self {
-                Times(r)
+                Times(TimesEnum::$type_name(r))
             }
         }
     };
