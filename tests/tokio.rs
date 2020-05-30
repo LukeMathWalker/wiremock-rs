4305a4d713b1db3c6be9e05c9f5380f9b83a4a04
Jeremy Lempereur <jeremy.lempereur@gmail.com>
2020-05-31 00:24:38 +0200

    fix 7 - runtime mixes. (#8)
    It was quite a fun one to track down. It turns out async runtimes mixing sometimes don't behave well.
    Here we're trying to make sure async_std is going to drive the mock server listener.

---
+++ tests/tokio.rs
@@ -0,0 +1,40 @@
+use reqwest::Client;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, MockServer, ResponseTemplate};
+
+// regression tests for https://github.com/LukeMathWalker/wiremock-rs/issues/7
+// running both tests will _sometimes_ trigger a hang if the runtimes arent separated correctly
+
+#[tokio::test]
+async fn hello_reqwest() {
+    let mock_server = MockServer::start().await;
+
+    Mock::given(method("GET"))
+        .and(path("/"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&mock_server)
+        .await;
+
+    println!("sending reqwest request");
+    let resp = Client::new().get(&mock_server.uri()).send().await.unwrap();
+    println!("reqwest request done");
+
+    assert_eq!(resp.status(), 200);
+}
+
+#[tokio::test]
+async fn hello_surf() {
+    let mock_server = MockServer::start().await;
+
+    Mock::given(method("GET"))
+        .and(path("/"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&mock_server)
+        .await;
+
+    println!("sending surf request");
+    let status = surf::get(&mock_server.uri()).await.unwrap().status();
+    println!("surf request done");
+
+    assert_eq!(status.as_u16(), 200);
+}
