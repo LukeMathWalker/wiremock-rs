503c148e4af01514c46450fb2f11b65e32465c1a
Yury Yarashevich <yura.yaroshevich@gmail.com>
2024-06-28 14:31:24 +0200

    Test http2 requests accepted.

--- tests/tokio.rs
+++ tests/tokio.rs
@@ -49,3 +49,26 @@ async fn hello_reqwest_actix() {
 
     assert_eq!(resp.status(), 200);
 }
+
+#[tokio::test]
+async fn hello_reqwest_http2() {
+    let mock_server = MockServer::start().await;
+
+    Mock::given(method("GET"))
+        .and(path("/"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&mock_server)
+        .await;
+
+    let resp = Client::builder()
+        .http2_prior_knowledge()
+        .build()
+        .expect("http client")
+        .get(&mock_server.uri())
+        .send()
+        .await
+        .expect("response");
+
+    assert_eq!(resp.status(), 200);
+    assert_eq!(resp.version(), reqwest::Version::HTTP_2);
+}
